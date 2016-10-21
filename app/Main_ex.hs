f0f4ef3202b9ef8fa91524ebba0dd65e4bb9f782
Takayuki UCHIDA <jacobitebeerlove@gmail.com>
2016-10-21 11:38:59 +0900

    add terminal setting

--- app/Main_ex.hs
+++ app/Main_ex.hs
@@ -1,11 +1,13 @@
 module Main where
 
 import Graphics.Gnuplot.Simple
-
+-- import qualified Graphics.Gnuplot.Terminal.X11 as X11
+import qualified Graphics.Gnuplot.Terminal.PostScript as PS
 
 main :: IO ()
 main = do
   let xs = (linearScale 1000 (0,2*pi)) :: [Double]
       ys = fmap sin xs
       points = zip xs ys
-  plotPath [(Title "hello")] points
+  -- plotPath [(terminal $ X11.cons)] points
+  plotPath [(terminal $ PS.cons "/home/ubuntu/temp.eps")] points
