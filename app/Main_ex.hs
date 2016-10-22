7e74333bb2b3fee25900788294b523b56ad8285b
Takayuki UCHIDA <jacobitebeerlove@gmail.com>
2016-10-23 04:47:06 +0900

    temporary change

--- app/Main_ex.hs
+++ app/Main_ex.hs
@@ -1,13 +1,22 @@
 module Main where
 
 import Graphics.Gnuplot.Simple
--- import qualified Graphics.Gnuplot.Terminal.X11 as X11
+import qualified Graphics.Gnuplot.Terminal.WXT as WXT
 import qualified Graphics.Gnuplot.Terminal.PostScript as PS
 
+
+ops :: [Attribute]
+-- ops = [(Custom "term" ["postscript", "eps", "enhanced", "color", "solid"])
+--       ,(Custom "output" ["\"/home/ubuntu/temp.eps\""])
+--       ]
+-- ops = [(terminal $ WXT.persist WXT.cons)]
+ops = [(EPS "/home/ubuntu/temp.eps")
+      ]
+
+  
 main :: IO ()
 main = do
   let xs = (linearScale 1000 (0,2*pi)) :: [Double]
       ys = fmap sin xs
       points = zip xs ys
-  -- plotPath [(terminal $ X11.cons)] points
-  plotPath [(terminal $ PS.cons "/home/ubuntu/temp.eps")] points
+  plotPath ops points
